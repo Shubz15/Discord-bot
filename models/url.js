4dd3726a45d29031d114d2787216a99c27898a16
shubz15 <shubhamshinde57509@gmail.com>
2026-08-05 09:28:07 +0530

    Updated code for URL Shortner

--- models/url.js
+++ models/url.js
@@ -1,31 +1,30 @@
 import mongoose from "mongoose";
 
-const urlSchema = new mongoose.Schema(
-  {
+const urlSchema = new mongoose.Schema({
+
     discordId: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
 
     originalUrl: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
 
     shortId: {
-      type: String,
-      required: true,
-      unique: true,
+        type: String,
+        required: true,
+        unique: true,
     },
 
     clicks: {
-      type: Number,
-      default: 0,
+        type: Number,
+        default: 0,
     },
-  },
-  {
+
+}, {
     timestamps: true,
-  }
-);
+});
 
 export default mongoose.model("Url", urlSchema);
