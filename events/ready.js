8f032aee63b932abe7b433a56a14cee3eb672ff8
shubz15 <shubhamshinde57509@gmail.com>
2026-08-11 18:43:48 +0530

    adding the feature for seeing all urls

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,6 @@ import { Events } from "discord.js";
 
 export default (client) => {
     client.once(Events.ClientReady, () => {
-        console.log(`✅ Logged in as ${client.user.tag}`);
+        console.log(`Logged in as ${client.user.tag}`);
     });
 };
