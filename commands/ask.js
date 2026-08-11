b7db43c2e436663cde09f88b31a787da3681a220
shubz15 <shubhamshinde57509@gmail.com>
2026-08-11 19:48:43 +0530

    add /ask command for gemini

---
+++ commands/ask.js
@@ -0,0 +1,40 @@
+import { SlashCommandBuilder } from "discord.js";
+import { askGemini } from "../services/gemini.service.js";
+
+export default {
+
+    data: new SlashCommandBuilder()
+        .setName("ask")
+        .setDescription("Ask Gemini a question")
+        .addStringOption(option =>
+            option
+                .setName("question")
+                .setDescription("Enter your question")
+                .setRequired(true)
+        ),
+
+    async execute(interaction) {
+
+        await interaction.deferReply();
+
+        try {
+
+            const question = interaction.options.getString("question");
+
+            const answer = await askGemini(question);
+
+            await interaction.editReply(answer);
+
+        } catch (error) {
+
+            console.error(error);
+
+            await interaction.editReply(
+                "Unable to get a response from Gemini."
+            );
+
+        }
+
+    }
+
+};
